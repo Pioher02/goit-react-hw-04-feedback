207872a18cfe37d4799f78ad0b3ac4214fafb359
Pioher02 <dpaola.hernandezr@gmail.com>
2023-06-06 21:20:13 -0500

    Todos los componentes refactorizados

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,20 +1,10 @@
-import { Component } from 'react';
-
-class Section extends Component {
-  static defaultProps = {
-    title: '',
-    children: {},
-  };
-  render() {
-    const { title } = this.props;
-    const { children } = this.props;
-    return (
-      <>
-        <h1>{title}</h1>
-        {children}
-      </>
-    );
-  }
-}
+const Section = ({ title, children }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      {children}
+    </>
+  );
+};
 
 export default Section;
