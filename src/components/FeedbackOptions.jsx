cc4b3e2b9bb7e1be2a74a0bf73bae7b9d2b32aa8
Pioher02 <dpaola.hernandezr@gmail.com>
2023-06-06 21:06:37 -0500

    FeedbackOptions refactorizado

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,33 +1,22 @@
-import { Component } from 'react';
-
-class FeedbackOptions extends Component {
-  static defaultProps = {
-    options: {},
-    onLeaveFeedback: 0,
-  };
-  render() {
-    const { options } = this.props;
-    const nameButtons = Object.keys(options);
-    const { onLeaveFeedback } = this.props;
-
-    return (
-      <ul style={{ display: 'flex', padding: 0 }}>
-        {nameButtons.map((nameButton, index) => (
-          <li key={index} style={{ listStyleType: 'none', margin: '5px' }}>
-            <button
-              type="button"
-              onClick={() => {
-                onLeaveFeedback(nameButton);
-              }}
-              style={{ textTransform: 'capitalize' }}
-            >
-              {nameButton}
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+const FeedbackOptions = props => {
+  const nameButtons = Object.keys(props.options);
+  return (
+    <ul style={{ display: 'flex', padding: 0 }}>
+      {nameButtons.map((nameButton, index) => (
+        <li key={index} style={{ listStyleType: 'none', margin: '5px' }}>
+          <button
+            type="button"
+            onClick={() => {
+              props.onLeaveFeedback(nameButton);
+            }}
+            style={{ textTransform: 'capitalize' }}
+          >
+            {nameButton}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default FeedbackOptions;
